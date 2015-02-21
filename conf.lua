943ffb3e7bc1397ef483bf6bfe298d51edb6ed02
Tyler Neylon <tylerneylon@gmail.com>
2015-02-21 11:28:50 -0800

    Start with template files.

---
+++ conf.lua
@@ -0,0 +1,4 @@
+function love.conf(t)
+  t.title    = 'pong-love'
+  t.identity = 'pong-love'
+end
---
+++ main.lua
@@ -0,0 +1,19 @@
+--[[ pong-love/main.lua
+
+TODO Put a nice description here.
+
+--]]
+
+function love.load()
+  -- This function runs once and first.
+end
+
+function love.update(dt)
+  -- This function runs once every go.
+  -- Update state here; dt is the time delta in seconds.
+end
+
+function love.draw()
+  -- This function runs once every go.
+  -- Draw stuff here.
+end
