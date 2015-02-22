41cfbe2460c0ec53990b46a38a858cf7e581cc2d
Tyler Neylon <tylerneylon@gmail.com>
2015-02-21 16:30:28 -0800

    Add scoring and new balls.

--- main.lua
+++ main.lua
@@ -9,8 +9,9 @@ This is the classic game pong.
 
 -- We use a coordinate system where (0, 0) is the middle of the screen, (-1, -1)
 -- is the lower-left corner, and (1, 1) is the upper-right corner.
-local ball = {x = 0, y = 0, dx = -0.01, dy = 0.006, w = 0.02, h = 0.02}
 
+-- These will be set up in love.load.
+local ball
 local players  -- This will be set up in love.load.
 
 local blue = {0, 255, 255}
@@ -33,6 +34,16 @@ local function draw_rect(x, y, w, h, color)
   love.graphics.rectangle('fill', x, y, w, h)
 end
 
+local function draw_str(s, x, y, limit, align)
+  local win_w, win_h = love.graphics.getDimensions()
+  x, y = (x + 1) * win_w / 2, (y + 1) * win_h / 2
+  limit = limit * win_w / 2
+
+  if align == 'right' then x = x - limit end
+
+  love.graphics.printf(s, x, y, limit, align)
+end
+
 local function draw_center_line()
   local w = 0.03
   local num_bars = 12
@@ -46,6 +57,15 @@ end
 
 
 -- Supporting functions.
+
+local function new_ball()
+  local dx_sign = math.random(2) * 2 - 3
+  local dy_sign = math.random(2) * 2 - 3
+  ball = {x = 0, y = 0,
+          dx = 0.01  * dx_sign,
+          dy = 0.006 * dy_sign,
+          w = 0.02, h = 0.02}
+end
 
 local function sign(x)
   if x > 0 then return 1 end
@@ -64,6 +84,10 @@ end
 function Player:draw()
   local w, h = self.w, self.h
   draw_rect(self.x - w / 2, self.y - h / 2, w, h)
+
+  local score_str = tostring(self.score)
+  local align = sign(self.x) == 1 and 'right' or 'left'
+  draw_str(score_str, sign(self.x), 0.8, 0.2, align)
 end
 
 function Player:update(dt)
@@ -85,10 +109,16 @@ function Player:update(dt)
   end
 end
 
+function Player:score_up()
+  self.score = self.score + 1
+  new_ball()
+end
+
 
 -- Love-based functions.
 
 function love.load()
+  new_ball()
   players = {Player:new(-0.8), Player:new(0.8)}
 end
 
@@ -98,6 +128,9 @@ function love.update(dt)
 
   if ball.y < (-1 + ball.h / 2) then ball.dy = -1 * ball.dy end
   if ball.y > ( 1 - ball.h / 2) then ball.dy = -1 * ball.dy end
+
+  if ball.x >  1 then players[1]:score_up() end
+  if ball.x < -1 then players[2]:score_up() end
 
   -- Move the players. This also handles ball collisions.
   for _, p in pairs(players) do
