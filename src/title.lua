fd7a77c73747b242b7d7184067eded512d60845a
Tyler Neylon <tylerneylon@gmail.com>
2015-03-25 09:07:53 -0700

    GB presents ticks in with the beat.

--- src/title.lua
+++ src/title.lua
@@ -15,6 +15,7 @@ local title = {}
 
 local battle   = require 'battle'
 local draw     = require 'draw'
+local events   = require 'events'
 local font     = require 'font'
 local sounds   = require 'sounds'
 
@@ -23,18 +24,31 @@ local sounds   = require 'sounds'
 -- Internal globals.
 --------------------------------------------------------------------------------
 
+-- How many beats have passed in the song.
+local num_beats = 0
+
+local sec_per_beat = 0.416666666
+
 
 --------------------------------------------------------------------------------
 -- Internal functions.
 --------------------------------------------------------------------------------
 
--- TEMP to help debug the color callback in the font module
-local function gaarlicbread_color(let, num_let, grid, num_grid)
-  local colors = {draw.black, draw.cyan, draw.gray, draw.green, draw.yellow}
-  local c = colors[grid % (#colors) + 1]
-
-  return c
+local function count_a_beat()
+  num_beats = num_beats + 1
+  events.add(sec_per_beat, count_a_beat)
 end
+
+local function gaarlicbread_color(let, num_let, grid, num_grid)
+  local beats_of_let = {0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2, 2}
+  local beats_this_let = beats_of_let[let]
+  if beats_this_let < num_beats then
+    return draw.black
+  else
+    return draw.white
+  end
+end
+
 
 --------------------------------------------------------------------------------
 -- Public functions.
@@ -47,7 +61,10 @@ function title.draw()
   draw.rect_w_mid_pt(0, 0, 2, 2, draw.white)
 
   font.draw_str('gaarlicbread', 0,  0.2, 0.5, 0, gaarlicbread_color)
-  font.draw_str('presents',     0, -0.2, 0.5, 0, draw.gray)
+
+  local presents_color = draw.gray
+  if num_beats < 4 then presents_color = draw.white end
+  font.draw_str('presents',     0, -0.2, 0.5, 0, presents_color)
 end
 
 function title.keypressed(key, isrepeat)
@@ -65,6 +82,7 @@ end
 --------------------------------------------------------------------------------
 
 sounds.beatz01:play()
+events.add(sec_per_beat, count_a_beat)
 
 
 --------------------------------------------------------------------------------
